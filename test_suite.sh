f5b6aafa17fa916fce90f3b6bc9bf5d8c134615b
root <root@B3Infra5-jenkins.8.8.8.8>
2023-05-31 12:32:21 +0000

    modified:   test_suite.sh

--- test_suite.sh
+++ test_suite.sh
@@ -3,7 +3,7 @@
 # ===============
 # Test le script suite avec les valeurs 1, 2, 3 et vérifier que le résultat est bien 5
 echo "test unitaire du script suite.sh"
-resultat=$(./suite.sh 1 2 3)
+resultat=$(/github/suite.sh 1 2 3)
 
 if [[ $resultat -eq 5 ]]
 then
